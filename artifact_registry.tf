252512bd2553b1cfaf1b219620667e639c6531bf
yamato0211 <kumayama0211@gmail.com>
2023-12-10 18:38:32 +0900

    add: version manifest & artifact registry

---
+++ artifact_registry.tf
@@ -0,0 +1,6 @@
+resource "google_artifact_registry_repository" "plesio-repository" {
+  location      = "${var.gcp_region}"
+  repository_id = "${var.gcp_project_id}-repository"
+  description   = "plesio docker repository"
+  format        = "DOCKER"
+}
